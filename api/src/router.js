2ae828495d62a724b0a917dea209f7038e3bfe64
Tiziano Signo <tiziano.signo@gmail.com>
2020-10-30 23:52:18 +0100

    add routing to support file upload and to handle static file serve

---
+++ api/src/router.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: 'api/uploads/',
+    filename
+});
+
+const upload = multer({ fileFilter, storage });
+
+router.post('/upload', upload.single('photo'), (request, response) => {
+    if (request.hasOwnProperty('fileValidationError')) {
+        response.status(400).json({ error: request.fileValidationError });
+    } else {
+        response.status(201).json({ success: true });
+    }
+});
+
+function filename(request, file, callback) {
+    callback(null, file.originalname);
+}
+
+function fileFilter(request, file, callback) {
+    if (file.mimetype !== 'image/png') {
+        request.fileValidationError = 'Wrong file type';
+        callback(null, false, new Error('Wrong file type'));
+    } else {
+        callback(null, true);
+    }
+}
+
+module.exports = router;
